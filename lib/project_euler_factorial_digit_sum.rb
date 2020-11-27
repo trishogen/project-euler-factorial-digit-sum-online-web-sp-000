446d04bc8cdbd05918e8cc7e0358bf1cb61197b2
trishogen <patriciaogen@gmail.com>
2020-11-27 09:40:31 -0500

    commit tree 67b730f9a2e35aeee762d02e51d05c8badcc9715

--- lib/project_euler_factorial_digit_sum.rb
+++ lib/project_euler_factorial_digit_sum.rb
@@ -1,1 +1,19 @@
-# code your solution here
+def factorial(n)
+  if n == 1
+    return 1
+  else
+    return n *factorial(n-1)
+  end
+end
+
+def sum_of_digits(n)
+  digits = n.to_s.split("")
+
+  return digits.map{|digit| digit.to_i}.inject(0, :+)
+end
+
+def factorial_digit_sum(n)
+  product = factorial(n)
+
+  return sum_of_digits(product)
+end
